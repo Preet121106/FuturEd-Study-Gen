d0c97b1590033d39afbcd109e05071f0874009c6
Preet121106 <preetbiswas2006@gmail.com>
2025-04-27 19:51:46 +0530

    updated

--- configs/db.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import { BaseEnvironment } from "./BaseEnvironment";
-
-const env = new BaseEnvironment();
-
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL!);
-export const db = drizzle(sql);
